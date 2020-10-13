1e2569a972f43c80fba7ac37215fdd3e51651222
Genatula <genatula@protonmail.com>
2020-10-13 10:08:08 +0300

    Wrote RightBracketSequence (gotta test)

--- Homework_5/RightBracketsSequence.java
+++ Homework_5/RightBracketsSequence.java
@@ -1,5 +1,38 @@
 public class RightBracketsSequence {
 	public static void main(String[] args) {
-		// some code with recursion 
+		System.out.println(isRightSequence("(())"));
+		System.out.println(isRightSequence("(()"));
+		System.out.println(isRightSequence(")(())"));
+		System.out.println(isRightSequence("(())())"));
+
+	}
+
+	public static boolean isRightSequence(String sequence) {
+		return isRightSequence(sequence, 0);
+	}
+
+	public static boolean isRightSequence(String sequence, int flag) {
+		int tempFlag = flag;
+		if (sequence.charAt(0) == '(') {
+			tempFlag = tempFlag + 1;
+		}
+		else if (sequence.charAt(0) == ')') {
+			tempFlag = tempFlag - 1;
+		}
+
+		if (tempFlag < 0) {
+			return false;
+		}
+		else if (sequence.length() == 1) {
+			if (tempFlag > 0) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+		else {
+			return isRightSequence(sequence.substring(1), tempFlag);
+		}
 	}
 }
